a6d7174bef636a6e0516df14d2726810e305cb75
sebastiancorrales477@gmail.com <=>
2023-02-22 17:44:58 -0500

    replace Datetime to DateTime

--- SlnZoo/PryZoo/DTO/ItineraryDTO.cs
+++ SlnZoo/PryZoo/DTO/ItineraryDTO.cs
@@ -3,17 +3,17 @@ namespace Zoo.DTO
     public class ItineraryDTO
     {
         public int id { get; set; }
-        public Datetime init_datetime { get; set; }
-        public Datetime end_datetime { get; set; }
+        public DateTime init_DateTime { get; set; }
+        public DateTime end_DateTime { get; set; }
         public float distance { get; set; }
         public int visitors { get; set; }
         public User user { get; set; }
 
-        public ItineraryDTO(int id, Datetime init_datetime, Datetime end_datetime, float distance, int visitors, User user)
+        public ItineraryDTO(int id, DateTime init_datetime, DateTime end_datetime, float distance, int visitors, User user)
         {
             this.id = id;
-            this.init_datetime = init_datetime;
-            this.end_datetime = end_datetime;
+            this.init_DateTime = init_datetime;
+            this.end_DateTime = end_datetime;
             this.distance = distance;
             this.visitors = visitors;
             this.user = user;
--- SlnZoo/PryZoo/DTO/UserDTO.cs
+++ SlnZoo/PryZoo/DTO/UserDTO.cs
@@ -6,9 +6,9 @@ namespace Zoo.DTO
         public string name { get; set; }
         public string address { get; set; }
         public string phone { get; set; }
-        public Datetime date_entry { get; set; }
-        public UserType user_type { get; set; }
-        public User(int id, string name, string address, string phone, Datime date_entry, UserType user_type)
+        public DateTime date_entry { get; set; }
+        public UserTypeDTO user_type { get; set; }
+        public User(int id, string name, string address, string phone, DateTime date_entry, UserTypeDTO user_type)
         {
             this.id = id;
             this.name = name;
